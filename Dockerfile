907ae3a8e15c89ff3206c3988dabb690d9761cc9
Umar-Rehman <u.rehman_97@hotmail.co.uk>
2022-02-14 15:28:41 +0000

    jenkins test 22

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM python:3.7-alpine
-
-CMD ["python", "-c", "print('Hello there!')"]
+COPY app.py /
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 ##testing jenkins
-import queries
+# import queries
 
-SQL = queries.SQLQuery("SQL Server", ".\SQLExpress", "JenkinsGIT", "GITdata")
-SQL.insert_data_from_csv("./log.csv")
+# SQL = queries.SQLQuery("SQL Server", ".\SQLExpress", "JenkinsGIT", "GITdata")
+# SQL.insert_data_from_csv("./log.csv")
+
+print("Hello there!")
